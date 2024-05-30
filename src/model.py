b26d1728b07c58567e94b34fe45cbbdd23379717
Markram-WS <mark_AI01@hotmail.com>
2024-05-30 22:26:16 +0700

    rm tf training

--- src/model.py
+++ src/model.py
@@ -1,130 +1,122 @@
 from tensorflow import keras
 import tensorflow as tf
 
-
-class Sampling(layers.Layer):
-    def call(self, inputs):
-        z_mean, z_log_var = inputs
-        batch = tf.shape(z_log_var)[0]
-        dim = tf.shape(z_log_var)[1]
-        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
-
-
-class OptionsGenerator(keras.Model):
-    def __init__(self, encoder, decoder, max_len, **kwargs):
-        super().__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
-        self.property_prediction_layer = keras.layers.Dense(1)
-        self.max_len = max_len
-
-        self.train_total_loss_tracker = keras.metrics.Mean(name="train_total_loss")
-        self.val_total_loss_tracker = keras.metrics.Mean(name="val_total_loss")
-
-    def train_step(self, data):
-        adjacency_tensor, feature_tensor, qed_tensor = data[0]
-        graph_real = [adjacency_tensor, feature_tensor]
-        self.batch_size = tf.shape(qed_tensor)[0]
-        with tf.GradientTape() as tape:
-            z_mean, z_log_var, qed_pred, gen_adjacency, gen_features = self(
-                graph_real, training=True
-            )
-            graph_generated = [gen_adjacency, gen_features]
-            total_loss = self._compute_loss(
-                z_log_var, z_mean, qed_tensor, qed_pred, graph_real, graph_generated
-            )
-
-        grads = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-
-        self.train_total_loss_tracker.update_state(total_loss)
-        return {"loss": self.train_total_loss_tracker.result()}
-
-    def _compute_loss(
-        self, z_log_var, z_mean, qed_true, qed_pred, graph_real, graph_generated
+class RelationalGraphConvLayer(keras.layers.Layer):
+    def __init__(
+        self,
+        units=128,
+        activation="relu",
+        use_bias=False,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        **kwargs
     ):
+        super().__init__(**kwargs)
 
-        adjacency_real, features_real = graph_real
-        adjacency_gen, features_gen = graph_generated
+        self.units = units
+        self.activation = keras.activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = keras.initializers.get(kernel_initializer)
+        self.bias_initializer = keras.initializers.get(bias_initializer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+        self.bias_regularizer = keras.regularizers.get(bias_regularizer)
 
-        adjacency_loss = tf.reduce_mean(
-            tf.reduce_sum(
-                keras.losses.categorical_crossentropy(adjacency_real, adjacency_gen),
-                axis=(1, 2),
-            )
-        )
-        features_loss = tf.reduce_mean(
-            tf.reduce_sum(
-                keras.losses.categorical_crossentropy(features_real, features_gen),
-                axis=(1),
-            )
-        )
-        kl_loss = -0.5 * tf.reduce_sum(
-            1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), 1
-        )
-        kl_loss = tf.reduce_mean(kl_loss)
+    def build(self, input_shape):
+        bond_dim = input_shape[0][1]
+        atom_dim = input_shape[1][2]
 
-        property_loss = tf.reduce_mean(
-            keras.losses.binary_crossentropy(qed_true, qed_pred)
+        self.kernel = self.add_weight(
+            shape=(bond_dim, atom_dim, self.units),
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            trainable=True,
+            name="W",
+            dtype=tf.float32,
         )
 
-        graph_loss = self._gradient_penalty(graph_real, graph_generated)
-
-        return kl_loss + property_loss + graph_loss + adjacency_loss + features_loss
-
-    def _gradient_penalty(self, graph_real, graph_generated):
-        # Unpack graphs
-        adjacency_real, features_real = graph_real
-        adjacency_generated, features_generated = graph_generated
-
-        # Generate interpolated graphs (adjacency_interp and features_interp)
-        alpha = tf.random.uniform([self.batch_size])
-        alpha = tf.reshape(alpha, (self.batch_size, 1, 1, 1))
-        adjacency_interp = (adjacency_real * alpha) + (1 - alpha) * adjacency_generated
-        alpha = tf.reshape(alpha, (self.batch_size, 1, 1))
-        features_interp = (features_real * alpha) + (1 - alpha) * features_generated
-
-        # Compute the logits of interpolated graphs
-        with tf.GradientTape() as tape:
-            tape.watch(adjacency_interp)
-            tape.watch(features_interp)
-            _, _, logits, _, _ = self(
-                [adjacency_interp, features_interp], training=True
+        if self.use_bias:
+            self.bias = self.add_weight(
+                shape=(bond_dim, 1, self.units),
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                trainable=True,
+                name="b",
+                dtype=tf.float32,
             )
 
-        # Compute the gradients with respect to the interpolated graphs
-        grads = tape.gradient(logits, [adjacency_interp, features_interp])
-        # Compute the gradient penalty
-        grads_adjacency_penalty = (1 - tf.norm(grads[0], axis=1)) ** 2
-        grads_features_penalty = (1 - tf.norm(grads[1], axis=2)) ** 2
-        return tf.reduce_mean(
-            tf.reduce_mean(grads_adjacency_penalty, axis=(-2, -1))
-            + tf.reduce_mean(grads_features_penalty, axis=(-1))
+        self.built = True
+
+    def call(self, inputs, training=False):
+        adjacency, features = inputs
+        # Aggregate information from neighbors
+        x = tf.matmul(adjacency, features[:, None, :, :])
+        # Apply linear transformation
+        x = tf.matmul(x, self.kernel)
+        if self.use_bias:
+            x += self.bias
+        # Reduce bond types dim
+        x_reduced = tf.reduce_sum(x, axis=1)
+        # Apply non-linear transformation
+        return self.activation(x_reduced)
+
+
+def encoder(
+    gconv_units, latent_dim, 
+    avgVol_shape, currentP_shape, strikeP_shape, dte_shape, #-- frture --
+    dense_units, dropout_rate
+):
+    avgVol = keras.layers.Input(shape=avgVol_shape, name="avg.Volume")
+    currentP = keras.layers.Input(shape=currentP_shape, name="currentPrice")
+    strikeP = keras.layers.Input(shape=strikeP_shape, name="strikePrice")
+    dte = keras.layers.Input(shape=dte_shape, name="DateToExpired")
+
+    # Propagate through one or more graph convolutional layers
+    for units in gconv_units:
+        features_transformed = RelationalGraphConvLayer(units)(
+            [avgVol, currentP, strikeP, dte]
         )
+    # Reduce 2-D representation of molecule to 1-D
+    x = keras.layers.GlobalAveragePooling1D()(features_transformed)
 
-    def inference(self, batch_size):
-        z = tf.random.normal((batch_size, LATENT_DIM))
-        reconstruction_adjacency, reconstruction_features = model.decoder.predict(z)
-        # obtain one-hot encoded adjacency tensor
-        adjacency = tf.argmax(reconstruction_adjacency, axis=1)
-        adjacency = tf.one_hot(adjacency, depth=BOND_DIM, axis=1)
-        # Remove potential self-loops from adjacency
-        adjacency = tf.linalg.set_diag(adjacency, tf.zeros(tf.shape(adjacency)[:-1]))
-        # obtain one-hot encoded feature tensor
-        features = tf.argmax(reconstruction_features, axis=2)
-        features = tf.one_hot(features, depth=ATOM_DIM, axis=2)
-        return [
-            graph_to_molecule([adjacency[i].numpy(), features[i].numpy()])
-            for i in range(batch_size)
-        ]
+    # Propagate through one or more densely connected layers
+    for units in dense_units:
+        x = keras.layers.Dense(units, activation="relu")(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
 
-    def call(self, inputs):
-        z_mean, log_var = self.encoder(inputs)
-        z = Sampling()([z_mean, log_var])
+    z_mean = keras.layers.Dense(latent_dim, dtype="float32", name="z_mean")(x)
+    log_var = keras.layers.Dense(latent_dim, dtype="float32", name="log_var")(x)
 
-        gen_adjacency, gen_features = self.decoder(z)
+    encoder = keras.Model([avgVol, currentP, strikeP, dte], [z_mean, log_var], name="encoder")
 
-        property_pred = self.property_prediction_layer(z_mean)
+    return encoder
 
-        return z_mean, log_var, property_pred, gen_adjacency, gen_features
+
+def decoder(dense_units, dropout_rate, latent_dim, 
+            avgVol_shape, currentP_shape, strikeP_shape, dte_shape, #-- frture --
+            ):
+    latent_inputs = keras.Input(shape=(latent_dim,))
+
+    x = latent_inputs
+    for units in dense_units:
+        x = keras.layers.Dense(units, activation="tanh")(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+
+    # Map outputs of previous layer (x) to [continuous] adjacency tensors (x_adjacency)
+    x_adjacency = keras.layers.Dense(tf.math.reduce_prod(adjacency_shape))(x)
+    x_adjacency = keras.layers.Reshape(adjacency_shape)(x_adjacency)
+    # Symmetrify tensors in the last two dimensions
+    x_adjacency = (x_adjacency + tf.transpose(x_adjacency, (0, 1, 3, 2))) / 2
+    x_adjacency = keras.layers.Softmax(axis=1)(x_adjacency)
+
+    # Map outputs of previous layer (x) to [continuous] feature tensors (x_features)
+    x_features = keras.layers.Dense(tf.math.reduce_prod(feature_shape))(x)
+    x_features = keras.layers.Reshape(feature_shape)(x_features)
+    x_features = keras.layers.Softmax(axis=2)(x_features)
+
+    decoder = keras.Model(
+        latent_inputs, outputs=[x_adjacency, x_features], name="decoder"
+    )
+
+    return decoder
