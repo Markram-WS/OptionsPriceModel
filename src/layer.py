2d9fc67a6c46bd8a8bc9b22c379afd4bd6beb4dd
Markram-WS <mark_AI01@hotmail.com>
2024-06-23 14:01:06 +0700

    fix creat model full loop

--- src/layer.py
+++ src/layer.py
@@ -1,80 +1,20 @@
 from tensorflow import keras
 import tensorflow as tf
 from   src.utils import debug
-class RelationalGraphConvLayer(keras.layers.Layer):
-    def __init__(
-        self,
-        output_dim=128,
-        activation="relu",
-        use_bias=False,
-        kernel_initializer="glorot_uniform",
-        bias_initializer="zeros",
-        kernel_regularizer=None,
-        bias_regularizer=None,
-        **kwargs
-    ):
-        super().__init__(**kwargs)
-
-        self.units = output_dim
-        self.activation = keras.activations.get(activation)
-        self.use_bias = use_bias
-        self.kernel_initializer = keras.initializers.get(kernel_initializer)
-        self.bias_initializer = keras.initializers.get(bias_initializer)
-        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
-        self.bias_regularizer = keras.regularizers.get(bias_regularizer)
-
-    def build(self, input_shape):
-        feature_dim  = input_shape[0][-1]
-        self.kernel = self.add_weight(
-            shape=(feature_dim, self.units),
-            initializer=self.kernel_initializer,
-            regularizer=self.kernel_regularizer,
-            trainable=True,
-            name="W",
-            dtype=tf.float32,
-        )
-
-        if self.use_bias:
-            self.bias = self.add_weight(
-                shape=(feature_dim[0], 1, self.units),
-                initializer=self.bias_initializer,
-                regularizer=self.bias_regularizer,
-                trainable=True,
-                name="b",
-                dtype=tf.float32,
-            )
-
-        self.built = True
-
-    def call(self, features):
-        # Apply linear transformation
-        print(features)
-        print(self.kernel)
-        x = tf.matmul(features, self.kernel)
-        if self.use_bias:
-            x += self.bias
-        # Apply non-linear transformation
-        return self.activation(x)
 
 
 def encoder(
     latent_dim, 
     input_shape, 
-    gconv_units, 
     dense_units, 
     dropout_rate,
     use_bias = False,
     batch_size=32
 ):    
-    features_input = keras.layers.Input(shape=input_shape, batch_size=batch_size, name='features')
-
-    # Propagate through one or more graph convolutional layers
-    for units in gconv_units:
-        features_transformed = RelationalGraphConvLayer(output_dim=units,use_bias=use_bias)(
-            [features_input]
-        )
+    features_input = keras.layers.Input(shape=input_shape,batch_size=batch_size , name='features') 
+    
     # Reduce 2-D representation of molecule to 1-D
-    x = keras.layers.GlobalAveragePooling1D()(features_transformed)
+    x = keras.layers.GlobalAveragePooling1D()(features_input)
 
     # Propagate through one or more densely connected layers
     for units in dense_units:
@@ -88,18 +28,16 @@ def encoder(
 
     return encoder
 
-
-def decoder(latent_dim, output_shape,dense_units,dropout_rate
-            ):
+def decoder(latent_dim, output_shape, dense_units, dropout_rate):
     latent_inputs = keras.Input(shape=(latent_dim,))
     
     decoder_outputs = {
-        "c_bid":None,
-        "c_ask":None,
-        "c_volume":None,
-        "p_bid":None,
-        "p_ask":None,
-        "p_volume":None
+        "c_bid": None,
+        "c_ask": None,
+        "c_volume": None,
+        "p_bid": None,
+        "p_ask": None,
+        "p_volume": None
     }
 
     x = latent_inputs
@@ -108,12 +46,12 @@ def decoder(latent_dim, output_shape,dense_units,dropout_rate
         x = keras.layers.Dropout(dropout_rate)(x)
 
     for k in decoder_outputs.keys():
-        decoder_outputs[k] = keras.layers.Dense(tf.math.reduce_prod(output_shape))(x)
+        decoder_outputs[k] = keras.layers.Dense(output_shape[0] * output_shape[1])(x)
         decoder_outputs[k] = keras.layers.Reshape(output_shape)(decoder_outputs[k])
-        decoder_outputs[k] = keras.layers.ReLU(axis=1)(decoder_outputs[k])
+        decoder_outputs[k] = keras.layers.ReLU()(decoder_outputs[k])
 
     decoder = keras.Model(
-        latent_inputs, outputs=[decoder_outputs.values()], name="decoder"
+        latent_inputs, outputs=list(decoder_outputs.values()), name="decoder"
     )
 
     return decoder
--- src/model.py
+++ src/model.py
@@ -12,10 +12,10 @@ class OptionChainGenerator(keras.Model):
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
         self.optVal_loss_tracker = keras.metrics.Mean(name="optVal_loss")
-        self.c_vol_loss_tracker = keras.metrics.Mean(name="c_vol_loss")
-        self.p_vol_loss_tracker = keras.metrics.Mean(name="p_vol_loss")
+        self.vol_loss_tracker = keras.metrics.Mean(name="vol_loss")
         
-    def train_step(self, X_input, Y_real):
+    def train_step(self, data):
+        X_input, Y_real = data  
 
         with tf.GradientTape() as tape:
             #__call__ objects
@@ -23,23 +23,23 @@ class OptionChainGenerator(keras.Model):
                 X_input, training=True
             )
             generated_data = [c_bid, c_ask, c_volume, p_bid, p_ask, p_volume]
-            total_loss,kl_loss  ,optVal_loss,p_volume_loss, c_volume_loss= self._compute_loss(
-                z_mean, z_log_var, Y_real[0], generated_data
+            total_loss,kl_loss  ,optVal_loss, volume_loss= self._compute_loss(
+                z_mean, z_log_var, Y_real, generated_data
             )
-            
+
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
 
         self.total_loss_tracker.update_state(total_loss)
         self.kl_loss_tracker.update_state(kl_loss)
         self.optVal_loss_tracker.update_state(optVal_loss)
-        self.c_vol_loss_tracker.update_state(c_volume_loss)
-        self.p_vol_loss_tracker.update_state(p_volume_loss)
+        self.vol_loss_tracker.update_state(volume_loss)
+
+    
         return {"total_loss": self.total_loss_tracker.result(),
                 "kl_loss": self.kl_loss_tracker.result(),
-                "optVal": self.optVal_loss_tracker.result(),
-                "c_vol": self.c_vol_loss_tracker.result(),
-                "p_vol": self.p_vol_loss_tracker.result()
+                "optVal_loss": self.optVal_loss_tracker.result(),
+                "vol_loss": self.vol_loss_tracker.result()
                 }
 
         
@@ -47,37 +47,34 @@ class OptionChainGenerator(keras.Model):
         features_real = Y_real
         # Reconstruction loss for each output
         reconstruction_losses = []
-        reconstruction_p_volume_losss = []
-        reconstruction_c_volume_losss = []
+        reconstruction_volume_losss = []
         reconstruction_values_losss = []
-        debug(features_real)
-        for input_name, decoder_output in zip(["c_bid", "c_ask", "c_volume", "p_bid", "p_ask", "p_volume"], generated_data[0]):
+        colList = ["c_bid", "c_ask", "c_volume", "p_bid", "p_ask", "p_volume"]
+        for input_name, decoder_output in zip(colList, generated_data):
             
             if input_name == "p_volume":
                 # Compute reconstruction loss for p_volume
                 #! features_input index of p_volume
-                reconstruction_loss_p_volume = tf.reduce_mean(tf.square(tf.reduce_sum(decoder_output, axis=1, keepdims=True)  - tf.reduce_sum(features_real[:, :, 0], axis=1, keepdims=True)))
-                reconstruction_p_volume_losss.append(reconstruction_loss_p_volume)
+                reconstruction_loss_p_volume = tf.reduce_mean(tf.square(tf.reduce_sum(decoder_output, axis=1, keepdims=True)  - tf.reduce_sum(features_real[:, :, -1], axis=1, keepdims=True)))
+                reconstruction_volume_losss.append(reconstruction_loss_p_volume)
                 reconstruction_losses.append(reconstruction_loss_p_volume)
                 
             elif input_name == "c_volume":
                 # Compute reconstruction loss for c_volume
                 #! features_input index of c_volume
-                reconstruction_loss_c_volume = tf.reduce_mean(tf.square(tf.reduce_sum(decoder_output, axis=1, keepdims=True)  - tf.reduce_sum(features_real[:, :, 0], axis=1, keepdims=True)))
-                reconstruction_c_volume_losss.append(reconstruction_loss_c_volume)
+                reconstruction_loss_c_volume = tf.reduce_mean(tf.square(tf.reduce_sum(decoder_output, axis=1, keepdims=True)  - tf.reduce_sum(features_real[:, :, -4], axis=1, keepdims=True)))
+                reconstruction_volume_losss.append(reconstruction_loss_c_volume)
                 reconstruction_losses.append(reconstruction_loss_c_volume)
                 
             else:
                 # Compute reconstruction loss for other features
-                reconstruction_loss = tf.reduce_mean(tf.square(decoder_output - features_real[:, :, 0]))
-                reconstruction_values_losss(reconstruction_loss)
+                reconstruction_loss = tf.reduce_mean(tf.square(tf.reduce_sum(decoder_output, axis=1, keepdims=True)  - tf.reduce_sum(features_real[:, :, colList.index(input_name)], axis=1, keepdims=True)))
+                reconstruction_values_losss.append(reconstruction_loss)
                 reconstruction_losses.append(reconstruction_loss)
-                
 
         # Reconstruction loss: sum of all reconstruction losses
         reconstruction_loss_total = tf.reduce_sum(reconstruction_losses)
-        reconstruction_p_volume_loss_total = tf.reduce_sum(reconstruction_p_volume_losss)
-        reconstruction_c_volume_loss_total = tf.reduce_sum(reconstruction_c_volume_losss)
+        reconstruction_volume_loss_total = tf.reduce_sum(reconstruction_volume_losss)
         reconstruction_values_losss = tf.reduce_sum(reconstruction_values_losss)
         # KL divergence loss
         kl_loss = -0.5 * tf.reduce_sum(1 + log_var - tf.square(z_mean) - tf.exp(log_var), axis=-1)
@@ -85,7 +82,7 @@ class OptionChainGenerator(keras.Model):
         # Total loss
         total_loss = tf.reduce_mean(reconstruction_loss_total + kl_loss)
 
-        return total_loss,kl_loss,reconstruction_values_losss,reconstruction_p_volume_loss_total,reconstruction_c_volume_loss_total
+        return total_loss,kl_loss,reconstruction_values_losss,reconstruction_volume_loss_total
 
     def call(self, inputs, training=False):
         z_mean, log_var = self.encoder(inputs)
