1a3f817eea48da8d61131d298fc87b76331dfb7f
Markram-WS <mark_AI01@hotmail.com>
2024-07-22 11:46:50 +0700

    checkpoint 16x

--- src/layer.py
+++ src/layer.py
@@ -13,8 +13,9 @@ def encoder(
     batch_size=32
 ):    
     features_input = keras.layers.Input(shape=input_shape,batch_size=batch_size , name='features') 
-    x = keras.layers.Reshape((batch_size, 2, 1))(features_input)  # Reshape to (32, 2, 1) for Conv2D
-
+    x = keras.layers.GlobalAveragePooling1D()(features_input)
+    #x = keras.layers.Reshape((input_shape[0], 2, 1))(features_input)  # Reshape to (input_shape[0], 2, 1) for Conv2D
+    #print(x)
     # Propagate through one or more densely connected layers
     for units in dense_units:
         #<NN>
@@ -28,8 +29,7 @@ def encoder(
         # #<CNN>
         # x = keras.layers.Conv2D(units, (3, 3), activation='relu', padding='same',kernel_initializer=initializer)(x)
         # x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    x = keras.layers.Flatten()(x)
-
+    #x = keras.layers.Flatten()(x)
     z_mean = keras.layers.Dense(
             latent_dim, 
             kernel_initializer=initializer,
@@ -59,9 +59,9 @@ def decoder(latent_dim,
     latent_inputs = keras.Input(shape=(latent_dim,))
     x = latent_inputs
     # สร้าง dense layer ตาม latent_dim
-    x = keras.layers.Dense(6 * latent_dim, activation='relu')(x)
+    #x = keras.layers.Dense(6 * latent_dim, activation='relu')(latent_inputs)
     # แปลง vector เป็นรูปแบบ 3D tensor
-    x = keras.layers.Reshape((6, latent_dim, 1))(x)
+    #x = keras.layers.Reshape((6, latent_dim, 1))(x)
     for units in dense_units:
         #<NN>
         x = keras.layers.Dense(units, 
@@ -78,10 +78,23 @@ def decoder(latent_dim,
         # x = keras.layers.UpSampling2D((2, 2))(x)
     
     #x = keras.layers.Conv2DTranspose(4, (3, 3), activation='relu', padding='same')(x)
-    x = keras.layers.Flatten()(x)
-    x = keras.layers.Dense(output_shape[0]*output_shape[1])(x)
+    # x = keras.layers.Flatten()(x)
+    # x = keras.layers.Dense(output_shape[0]*output_shape[1])(x)
     # สร้าง output layer โดยกำหนดให้มี shape (None, 4, 32)
-    decoder_outputs = keras.layers.Reshape(output_shape)(x)  # Reshape to (32, 4) to match output shape(x)
+    
+    ##<CNN>
+    #decoder_outputs = keras.layers.Reshape(output_shape)(x)  # Reshape to (32, 4) to match output shape(x)
+    
+    ##<NN>
+    ##-----sigle node
+    #x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(output_shape[0] * output_shape[1], 
+                           activation="linear",
+                           kernel_initializer=initializer,
+                           use_bias=use_bias,
+                           dtype="float32")(x)
+    decoder_outputs = keras.layers.Reshape(output_shape)(x)
+    ##-----munti node
     # decoder_outputs = {
     #     "c_bid": None,
     #     "c_ask": None,
